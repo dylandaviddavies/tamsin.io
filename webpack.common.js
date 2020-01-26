767796cd9186f66593152c24143f4230e508b04c
dylandaviddavies <dylandaviddavies@gmail.com>
2020-01-26 01:36:37 -0500

    Uninstall social-tags-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SocialTags = require('social-tags-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -25,25 +24,6 @@ module.exports = {
                 'viewport': 'width=device-width,initial-scale=1,shrink-to-fit=no',
                 'theme-color': '#1d1d1d',
                 'description': 'Vue+Pug+Sass mockup by Dylan Davies'
-            }
-        }),
-        new SocialTags({
-            appUrl: 'ttps://dylandaviddavies.github.io/tamsin.io',
-            facebook: {                
-                'og:url': 'https://dylandaviddavies.github.io/tamsin.io',
-                'og:title': 'tamsin.io',
-                'og:description': 'Vue+Pug+Sass mockup by Dylan Davies',
-                'og:site_name': 'tamsin.io',
-                'og:image': '@/assets/thumbnail.png',
-                'fb:app_id': '1345605492308388',
-                'og:type': 'website',
-                'og:locale': 'en_CA',
-            },
-            twitter: {
-                'twitter:card': 'summary',
-                'twitter:title': 'tamsin.io',
-                'twitter:description': 'Vue+Pug+Sass Mockup',
-                'twitter:image': '@/assets/thumbnail.png'
             }
         })
     ],
